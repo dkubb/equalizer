e28921875ae0294d04a3b39200f158b504341e6a
Dan Kubb <dan.kubb@gmail.com>
2012-12-24 23:49:48 -0800

    Fix spelling mistake

--- spec/unit/equalizer/methods/equal_value_spec.rb
+++ spec/unit/equalizer/methods/equal_value_spec.rb
@@ -49,7 +49,7 @@ describe Equalizer::Methods, '#==' do
     it { should be(true) }
 
     it 'is not symmetric' do
-      # the subclass instance should mainstain substitutability with the object
+      # the subclass instance should maintain substitutability with the object
       # (in the LSP sense) the reverse is not true.
       should_not eql(other == object)
     end
